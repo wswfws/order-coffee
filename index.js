a3f76ec1d10efbf3d2f0bede723d333b6f733608
wswfws <wswfws@ya.ru>
2025-04-22 18:06:55 +0500

    done 4

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-const template = document.getElementById("beverage");
+const template = document.getElementById("beverage-template");
 const orderCoffee = document.getElementById("order-coffee");
 
 
-let items = 1;
+let item_key = 1;
+
 
 const addOrderItem = () => {
     const clone = template.cloneNode(true);
     clone.style.display = "block";
-    clone.classList.add(`item-${items}`);
+    clone.classList.add(`item-${item_key}`);
     orderCoffee.insertBefore(clone, orderCoffee.firstChild);
 
-    const name = document.querySelector(`.item-${items} > h4`);
-    name.textContent = `Напиток №${items}`;
-    items++;
+    const name = document.querySelector(`.item-${item_key} > h4`);
+    name.textContent = `Напиток №${item_key}`;
+    item_key++;
 }
 
 document.getElementById("add-button").addEventListener("click", addOrderItem);
@@ -26,11 +27,18 @@ const orderMessage = modal.querySelector('#orderMessage');
 form.addEventListener('submit', e => {
     e.preventDefault();
     orderMessage.textContent = `Заказ принят!`;
+    getOrderItems()
     modal.classList.remove('hidden');
 });
 
 function hideModal() {
     modal.classList.add('hidden');
+}
+
+const getOrderItems = () => {
+    form.childNodes.forEach((item) => {
+        console.log(item);
+    })
 }
 
 closeCross.addEventListener('click', hideModal);
