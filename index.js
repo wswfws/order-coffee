e3551d76bf6dca311b5213e03339014f379195d7
wswfws <wswfws@ya.ru>
2025-04-22 17:58:40 +0500

    done 3

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const orderCoffee = document.getElementById("order-coffee");
 
 let items = 1;
 
-const addOrderItem = ()=> {
+const addOrderItem = () => {
     const clone = template.cloneNode(true);
     clone.style.display = "block";
     clone.classList.add(`item-${items}`);
@@ -16,3 +16,22 @@ const addOrderItem = ()=> {
 }
 
 document.getElementById("add-button").addEventListener("click", addOrderItem);
+
+const form = document.querySelector('form');
+const modal = document.getElementById('orderModal');
+const closeCross = modal.querySelector('.modal-close');
+const overlay = modal.querySelector('.modal-overlay');
+const orderMessage = modal.querySelector('#orderMessage');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    orderMessage.textContent = `Заказ принят!`;
+    modal.classList.remove('hidden');
+});
+
+function hideModal() {
+    modal.classList.add('hidden');
+}
+
+closeCross.addEventListener('click', hideModal);
+overlay.addEventListener('click', hideModal);
